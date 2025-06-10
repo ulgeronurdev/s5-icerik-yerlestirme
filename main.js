0d1fa7bfe02167b09a175e63d6491e694c445353
Onur Ülger <ulgeronur@outlook.com>
2025-06-11 00:17:53 +0300

    5 passed

--- main.js
+++ main.js
@@ -3,3 +3,27 @@ import { siteContent } from "./content.js";
 console.log("Site içeriği", siteContent);
 
 /* Kodlar buradan aşağıya */
+
+const navLinks = document.querySelectorAll("nav a");
+const navData = siteContent["nav"];
+
+Object.keys(navData).forEach((key, index) => {
+  navLinks[index].textContent = navData[key];
+});
+
+const logo = document.getElementById("logo-img");
+logo.src = siteContent.images["logo-img"];
+
+const ctaH1 = document.querySelector(".cta h1");
+ctaH1.textContent = siteContent.cta.h1;
+
+const ctaBtn = document.querySelector(".cta button");
+ctaBtn.textContent = siteContent.cta.button;
+
+const ctaImg = document.getElementById("cta-img");
+ctaImg.src = siteContent.images["cta-img"];
+
+const topContent = document.querySelectorAll(".top-content .text-content");
+const bottomContent = document.querySelectorAll(
+  ".bottom-content .text-content"
+);
