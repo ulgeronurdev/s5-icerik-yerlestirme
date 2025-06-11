febbe116da32ad8e9b96888b7c33d3998f50848f
Onur Ülger <ulgeronur@outlook.com>
2025-06-11 09:55:11 +0300

    final

--- main.js
+++ main.js
@@ -26,3 +26,44 @@ ctaImg.src = siteContent.images["cta-img"];
 
 const middleImg = document.getElementById("middle-img");
 middleImg.src = siteContent.images["accent-img"];
+
+const topContentHeaders = document.querySelectorAll(
+  ".top-content .text-content h4"
+);
+const topContentP = document.querySelectorAll(".top-content .text-content p");
+
+topContentHeaders[0].innerHTML = siteContent["top-content"]["left-h4"];
+topContentP[0].textContent = siteContent["top-content"]["left-content"];
+
+topContentHeaders[1].innerHTML = siteContent["top-content"]["right-h4"];
+topContentP[1].textContent = siteContent["top-content"]["right-content"];
+
+const bottomContentHeaders = document.querySelectorAll(
+  ".bottom-content .text-content h4"
+);
+const bottomContentParagraphs = document.querySelectorAll(
+  ".bottom-content .text-content p"
+);
+
+bottomContentHeaders[0].textContent = siteContent["bottom-content"]["left-h4"];
+bottomContentParagraphs[0].textContent =
+  siteContent["bottom-content"]["left-content"];
+
+bottomContentHeaders[1].textContent =
+  siteContent["bottom-content"]["middle-h4"];
+bottomContentParagraphs[1].textContent =
+  siteContent["bottom-content"]["middle-content"];
+
+bottomContentHeaders[2].textContent = siteContent["bottom-content"]["right-h4"];
+bottomContentParagraphs[2].textContent =
+  siteContent["bottom-content"]["right-content"];
+
+const contactSection = document.querySelector(".contact");
+contactSection.children[0].textContent = siteContent.contact["contact-h4"];
+contactSection.children[1].textContent = siteContent.contact.address;
+contactSection.children[2].textContent = siteContent.contact.phone;
+contactSection.children[3].textContent = siteContent.contact.email;
+
+const footerLink = document.querySelector("footer a");
+footerLink.textContent = siteContent.footer.copyright;
+footerLink.classList.add("bold");
